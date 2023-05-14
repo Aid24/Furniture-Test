3efdd1b24be1b202a149d634e06821bf4f87739a
Aid24 <105000149+Aid24@users.noreply.github.com>
2023-05-14 19:51:34 +0300

    first commit

---
+++ src/js/scripts.js
@@ -0,0 +1,8 @@
+// Custom Scripts
+@@include('./libs/swiper(lib).js')
+@@include('./libs/gsap(lib).min.js')
+@@include('./libs/ScrollTrigger(lib).min.js')
+@@include('./libs/smooth-scroll.min(lib).js')
+@@include('main.js')
+
+
